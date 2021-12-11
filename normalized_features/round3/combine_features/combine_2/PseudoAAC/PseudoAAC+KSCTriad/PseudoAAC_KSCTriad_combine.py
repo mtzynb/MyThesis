b5b9a5990282f9512602e545539693f00c18bb0b
mtzynb <zeynab.mohammadtabar@gmail.com>
2021-12-11 10:42:11 +0330

    - add 2 combined features sofar

---
+++ normalized_features/round3/combine_features/combine_2/PseudoAAC/PseudoAAC+KSCTriad/PseudoAAC_KSCTriad_combine.py
@@ -0,0 +1,24 @@
+from codes.tsv_file_utils import get_data_frame_from_tsv_file, write_data_frame_to_tsv_file
+import pandas as pd
+
+# ------------ input files -------------
+
+KSCTriad_90 = '../../../../../../modify_features_column/round3/one_fearure/KSCTriad/KSCTriad_90_mixed_normalized_modified_cols_name.tsv'
+PseudoAAC_90 = '../../../../../../modify_features_column/round3/one_fearure/PseudoAAC/PseudoAAC_90_mixed_normalized_modified_cols_name.tsv'
+
+# ------------ output files -------------
+combined_features_out = 'PseudoAAC_KSCTriad_combined_90_mixed_normalized.csv'
+# ---------------------------------------
+# get df from cdhit 90 data
+KSCTriad_90_df = get_data_frame_from_tsv_file(KSCTriad_90)  # acc= 0.77
+PseudoAAC_90_df = get_data_frame_from_tsv_file(PseudoAAC_90)  # acc= 0.79
+# ---------------------------------------
+# concat all features
+combined_features_df = pd.concat([PseudoAAC_90_df,
+                                  KSCTriad_90_df.iloc[:, 1:]
+                                  ]
+                                 , axis=1)
+# ---------------------------------------
+write_data_frame_to_tsv_file(combined_features_df, combined_features_out)
+
+print(combined_features_df.shape)  # (940, 715)
