6091f85c2297ad5af3210d7328c490c83324c320
mtzynb <zeynab.mohammadtabar@gmail.com>
2021-12-16 01:01:16 +0330

    - remove round2 files

--- data/split_data/round2/random_neg_data/count.py
+++
@@ -1,5 +0,0 @@
-from codes.preprocess_dataaa import count_seq_samples
-
-print("ACP_80_neg_random: ", count_seq_samples("ACP_80_neg_random.fasta"))
-print("ACP_80_neg_random: ", count_seq_samples("ACP_90_neg_random.fasta"))
-print("ACP_80_neg_random: ", count_seq_samples("ACP_100_neg_random.fasta"))
